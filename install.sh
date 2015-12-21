28caff37ec09f4586dd2f148f6d715f53810fcf8
cbweaver <cashbweaver@gmail.com>
2015-12-21 10:38:59 -0800

    Relocate existing .bash_aliases

--- install.sh
+++ install.sh
@@ -3,16 +3,21 @@
 HOME_PATH="/home/$(whoami)"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "Installing bash_config"
+echo "Installing bash configuration"
 
-echo "  Creating backups"
+echo "  Reorganizing existing bashrc and bash_alias files"
 if [[ -f $HOME_PATH/.bash_aliases ]]; then
-    mv $HOME_PATH/.bash_aliases $HOME_PATH/.bash_aliases.bak
-    echo "    $HOME_PATH/.bash_aliases backed up to $HOME_PATH/.bash_aliases.bak"
+  if [[ -f $HOME_PATH/.bash_aliases.local ]]; then
+    mv $HOME_PATH/.bash_aliases.local $HOME_PATH/.bash_aliases.local.bak
+    echo "    $HOME_PATH/.bash_aliases.local backed up to $HOME_PATH/.bash_aliases.local.bak"
+  fi
+
+  mv $HOME_PATH/.bash_aliases $HOME_PATH/.bash_aliases.local
+  echo "    $HOME_PATH/.bash_aliases moved up to $HOME_PATH/.bash_aliases.local"
 fi
 if [[ -f $HOME_PATH/.bashrc ]]; then
-    mv $HOME_PATH/.bashrc $HOME_PATH/.bashrc.bak
-    echo "    $HOME_PATH/.bashrc backed up to $HOME_PATH/.bashrc.bak"
+  mv $HOME_PATH/.bashrc $HOME_PATH/.bashrc.bak
+  echo "    $HOME_PATH/.bashrc backed up to $HOME_PATH/.bashrc.bak"
 fi
 
 echo "  Creating symlinks"
